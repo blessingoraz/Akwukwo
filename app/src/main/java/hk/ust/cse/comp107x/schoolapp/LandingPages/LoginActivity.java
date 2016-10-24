2e4ddabb1688b91afd4fd7e9bcaf23c366fe50a1
omolara <larryade92@gmail.com>
2016-10-25 00:37:04 +0100

    [Fix] checks if the email and password fields are not empty

--- app/src/main/java/hk/ust/cse/comp107x/schoolapp/LandingPages/LoginActivity.java
+++ app/src/main/java/hk/ust/cse/comp107x/schoolapp/LandingPages/LoginActivity.java
@@ -5,133 +5,133 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.AuthData;
-import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
 import hk.ust.cse.comp107x.schoolapp.Constants;
 import hk.ust.cse.comp107x.schoolapp.R;
 import hk.ust.cse.comp107x.schoolapp.ResetPasswordActivity;
-import hk.ust.cse.comp107x.schoolapp.Singletons.UserDetails;
 import hk.ust.cse.comp107x.schoolapp.Singletons.Utils;
 import hk.ust.cse.comp107x.schoolapp.ViewPageActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText mUserLoginEmail, mUserLoginPassword;
-    private ProgressDialog mProgress;
-    Firebase ref;
-    SharedPreferences pref;
+	private EditText mUserLoginEmail, mUserLoginPassword;
+	private ProgressDialog mProgress;
+	Firebase ref;
+	SharedPreferences pref;
 
-    private Boolean exit = false;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_acitivity);
+	private Boolean exit = false;
 
-        Firebase.setAndroidContext(this);
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_login_acitivity);
 
-        mUserLoginEmail = (EditText) findViewById(R.id.user_email_login);
-        mUserLoginPassword = (EditText) findViewById(R.id.user_password_login);
+		Firebase.setAndroidContext (this);
 
-        mUserLoginPassword.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction() == KeyEvent.ACTION_DOWN) {
-                    switch (keyCode){
-                        case KeyEvent.KEYCODE_DPAD_CENTER:
-                        case KeyEvent.KEYCODE_ENTER:
-                            Utils.hideSoftKeyboard(LoginActivity.this);
-                            return true;
-                        default:
-                            break;
-                    }
+		mUserLoginEmail = (EditText) findViewById (R.id.user_email_login);
+		mUserLoginPassword = (EditText) findViewById (R.id.user_password_login);
 
-                }
-                return false;
-            }
-        });
+		mUserLoginPassword.setOnKeyListener (new View.OnKeyListener () {
+			@Override
+			public boolean onKey (View v, int keyCode, KeyEvent event) {
+				if (event.getAction () == KeyEvent.ACTION_DOWN) {
+					switch (keyCode) {
+						case KeyEvent.KEYCODE_DPAD_CENTER:
+						case KeyEvent.KEYCODE_ENTER:
+							Utils.hideSoftKeyboard (LoginActivity.this);
+							return true;
+						default:
+							break;
+					}
 
-        pref = getSharedPreferences("Email", Context.MODE_PRIVATE);
+				}
+				return false;
+			}
+		});
 
-        String emailFromSharedPref = pref.getString("email", "");
+		pref = getSharedPreferences ("Email", Context.MODE_PRIVATE);
 
-        mUserLoginEmail.setText(emailFromSharedPref);
-    }
+		String emailFromSharedPref = pref.getString ("email", "");
 
-    public void login(View view){
+		mUserLoginEmail.setText (emailFromSharedPref);
+	}
 
-        if(Utils.isOnLine(LoginActivity.this)) {
+	public void login (View view) {
+		String email = mUserLoginEmail.getText ().toString ();
+		String password = mUserLoginPassword.getText ().toString ();
+		if (Utils.isEmpty (email)) {
+			mUserLoginEmail.setError ("This field is required");
+		} else if (Utils.isEmpty (password)) {
+			mUserLoginPassword.setError ("This field is required");
+		} else if (Utils.isNotEmpty (email) && Utils.isNotEmpty (password)) {
+			if (Utils.isOnLine (LoginActivity.this)) {
 
-            mProgress = ProgressDialog.show(LoginActivity.this, "", getString(R.string.loading), true, false);
-            ref = new Firebase(Constants.FIREBASE_URL_USERS);
-            String email = mUserLoginEmail.getText().toString();
-            String password = mUserLoginPassword.getText().toString();
+				mProgress = ProgressDialog.show (LoginActivity.this, "", getString (R.string.loading), true, false);
+				ref = new Firebase (Constants.FIREBASE_URL_USERS);
 
-            ref.authWithPassword(email, password, new Firebase.AuthResultHandler() {
-                @Override
-                public void onAuthenticated(AuthData authData) {
 
-                    if (mProgress != null)
-                        mProgress.dismiss();
+				ref.authWithPassword (email, password, new Firebase.AuthResultHandler () {
+					@Override
+					public void onAuthenticated (AuthData authData) {
 
-                    startActivity(new Intent(LoginActivity.this, ViewPageActivity.class));
-                }
+						if (mProgress != null)
+							mProgress.dismiss ();
 
-                @Override
-                public void onAuthenticationError(FirebaseError firebaseError) {
+						startActivity (new Intent (LoginActivity.this, ViewPageActivity.class));
+					}
 
-                    if (mProgress != null)
-                        mProgress.dismiss();
+					@Override
+					public void onAuthenticationError (FirebaseError firebaseError) {
 
-                    switch (firebaseError.getCode()) {
-                        case FirebaseError.USER_DOES_NOT_EXIST:
-                            // handle a non existing user
-                            Toast.makeText(LoginActivity.this, "User does not exist", Toast.LENGTH_SHORT).show();
+						if (mProgress != null)
+							mProgress.dismiss ();
 
-                            break;
-                        case FirebaseError.INVALID_PASSWORD:
-                            // handle an invalid password
-                            Toast.makeText(LoginActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show();
-                            break;
-                        default:
-                            // handle other errors
-                            break;
-                    }
-                }
-            });
+						switch (firebaseError.getCode ()) {
+							case FirebaseError.USER_DOES_NOT_EXIST:
+								// handle a non existing user
+								Toast.makeText (LoginActivity.this, "User does not exist", Toast.LENGTH_SHORT).show ();
 
-            SharedPreferences.Editor editor = pref.edit();
-            editor.clear();
-            editor.commit();
+								break;
+							case FirebaseError.INVALID_PASSWORD:
+								// handle an invalid password
+								Toast.makeText (LoginActivity.this, "Password is incorrect", Toast.LENGTH_SHORT).show ();
+								break;
+							default:
+								// handle other errors
+								break;
+						}
+					}
+				});
 
-        } else {
-            Utils.showLongMessage(Constants.CHECK_CONNECTION, LoginActivity.this);
-        }
+				SharedPreferences.Editor editor = pref.edit ();
+				editor.clear ();
+				editor.commit ();
 
-    }
+			} else {
+				Utils.showLongMessage (Constants.CHECK_CONNECTION, LoginActivity.this);
+			}
+		}
+	}
 
-    @Override
-    public void onBackPressed() {
+	@Override
+	public void onBackPressed () {
 
-        finish();
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
+		finish ();
+		Intent intent = new Intent (LoginActivity.this, MainActivity.class);
+		startActivity (intent);
+	}
 
-    public void forgotPassword(View view) {
+	public void forgotPassword (View view) {
 
-        startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
-    }
+		startActivity (new Intent (LoginActivity.this, ResetPasswordActivity.class));
+	}
 }
--- app/src/main/java/hk/ust/cse/comp107x/schoolapp/LandingPages/SignUpPageActivity.java
+++ app/src/main/java/hk/ust/cse/comp107x/schoolapp/LandingPages/SignUpPageActivity.java
@@ -1,10 +1,10 @@
 package hk.ust.cse.comp107x.schoolapp.LandingPages;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -28,186 +28,181 @@ import hk.ust.cse.comp107x.schoolapp.ViewPageActivity;
 
 public class SignUpPageActivity extends AppCompatActivity {
 
-    private TextView mUserName;
-    private TextView mUserPassword;
-    private TextView mUserEmail;
-    Firebase ref = new Firebase(Constants.FIREBASE_URL);
-    final Firebase userRef = ref.child("users");
+	private TextView mUserName;
+	private TextView mUserPassword;
+	private TextView mUserEmail;
+	Firebase ref = new Firebase (Constants.FIREBASE_URL);
+	final Firebase userRef = ref.child ("users");
 
-    private ProgressDialog mProgress;
+	private ProgressDialog mProgress;
 
-    @Override
-    public void onBackPressed() {
+	@Override
+	public void onBackPressed () {
 
-        finish();
-        Intent intent = new Intent(SignUpPageActivity.this, MainActivity.class);
-        startActivity(intent);
-    }
+		finish ();
+		Intent intent = new Intent (SignUpPageActivity.this, MainActivity.class);
+		startActivity (intent);
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_page);
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView (R.layout.activity_sign_up_page);
 
-        Firebase.setAndroidContext(this);
+		Firebase.setAndroidContext (this);
 
-        mUserEmail = (TextView) findViewById(R.id.user_email);
-        mUserName = (TextView) findViewById(R.id.user_name);
-        mUserPassword = (TextView) findViewById(R.id.user_password);
+		mUserEmail = (TextView) findViewById (R.id.user_email);
+		mUserName = (TextView) findViewById (R.id.user_name);
+		mUserPassword = (TextView) findViewById (R.id.user_password);
 
-        mUserPassword.setOnKeyListener(new View.OnKeyListener() {
-            @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction() == KeyEvent.ACTION_DOWN) {
-                    switch (keyCode){
-                        case KeyEvent.KEYCODE_DPAD_CENTER:
-                        case KeyEvent.KEYCODE_ENTER:
-                            Utils.hideSoftKeyboard(SignUpPageActivity.this);
-                            return true;
-                        default:
-                            break;
-                    }
+		mUserPassword.setOnKeyListener (new View.OnKeyListener () {
+			@Override
+			public boolean onKey (View v, int keyCode, KeyEvent event) {
+				if (event.getAction () == KeyEvent.ACTION_DOWN) {
+					switch (keyCode) {
+						case KeyEvent.KEYCODE_DPAD_CENTER:
+						case KeyEvent.KEYCODE_ENTER:
+							Utils.hideSoftKeyboard (SignUpPageActivity.this);
+							return true;
+						default:
+							break;
+					}
 
-                }
-                return false;
-            }
-        });
-    }
+				}
+				return false;
+			}
+		});
+	}
 
-    public void signUp(View view) {
+	public void signUp (View view) {
 
-        if(Utils.isOnLine(SignUpPageActivity.this)) {
+		if (Utils.isOnLine (SignUpPageActivity.this)) {
 
-            SharedPreferences pref = getSharedPreferences("UserDetails", MODE_PRIVATE);
+			SharedPreferences pref = getSharedPreferences ("UserDetails", MODE_PRIVATE);
 
-            final SharedPreferences.Editor editor = pref.edit();
+			final SharedPreferences.Editor editor = pref.edit ();
 
-            final String email = mUserEmail.getText().toString().trim();
-            final String name = mUserName.getText().toString().trim();
-            final String password = mUserPassword.getText().toString().trim();
+			final String email = mUserEmail.getText ().toString ().trim ();
+			final String name = mUserName.getText ().toString ().trim ();
+			final String password = mUserPassword.getText ().toString ().trim ();
 
-            if(Utils.isEmpty(email)) {
-                mUserEmail.setError("This field is required");
-            }
+			if (Utils.isEmpty (email)) {
+				mUserEmail.setError ("This field is required");
+			} else if (Utils.isEmpty (name)) {
+				mUserName.setError ("This field is required");
+			} else if (Utils.isEmpty (password)) {
+				mUserPassword.setError ("This field is required");
+			} else if (!isPasswordValid (password)) {
+				mUserPassword.setError ("Password is too short");
+				mUserPassword.setHint ("input password");
+			}
 
-            else if(Utils.isEmpty(name)) {
-                mUserName.setError("This field is required");
-            }
-            else if(Utils.isEmpty(password)) {
-                mUserPassword.setError("This field is required");
-            }
+			// Remove this
 
-            else if(!isPasswordValid(password)) {
-                mUserPassword.setError( "Password is too short" );
-                mUserPassword.setHint("input password");
-            }
+			else if (Utils.isNotEmpty (email) && Utils.isNotEmpty (name) && Utils.isNotEmpty (password)) {
 
-            // Remove this
-            
-            else if(Utils.isNotEmpty(email) && Utils.isNotEmpty(name) && Utils.isNotEmpty(password)) {
+				mProgress = ProgressDialog.show (
+						SignUpPageActivity.this, "", getString (R.string.loading), true, false);
+				ref.createUser (email, password, new Firebase.ValueResultHandler<Map<String, Object>> () {
+					@Override
+					public void onSuccess (Map<String, Object> result) {
 
-                mProgress = ProgressDialog.show(
-                        SignUpPageActivity.this, "", getString(R.string.loading), true, false);
-                ref.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
-                    @Override
-                    public void onSuccess(Map<String, Object> result) {
+						if (mProgress != null)
+							mProgress.dismiss ();
 
-                        if (mProgress != null)
-                            mProgress.dismiss();
+						UserDetails userDetails = new UserDetails ();
+						userDetails.setName (name);
+						userDetails.setEmail (email);
+						userDetails.setPassword (password);
+						userDetails.setAccessToken ((String) result.get ("uid"));
 
-                        UserDetails userDetails = new UserDetails();
-                        userDetails.setName(name);
-                        userDetails.setEmail(email);
-                        userDetails.setPassword(password);
-                        userDetails.setAccessToken((String) result.get("uid"));
+						String uid = (String) result.get ("uid");
 
-                        String uid = (String) result.get("uid");
+						editor.putString (Constants.USER_TOKEN, uid);
+						editor.putString (Constants.USER_NAME, name);
+						editor.putString (Constants.USER_EMAIL, email);
 
-                        editor.putString(Constants.USER_TOKEN, uid);
-                        editor.putString(Constants.USER_NAME, name);
-                        editor.putString(Constants.USER_EMAIL, email);
+						editor.apply ();
 
-                        editor.apply();
+						compareEmail (userDetails, userDetails.email, uid);
 
-                        compareEmail(userDetails, userDetails.email, uid);
+						alertDialog ();
+					}
 
-                        alertDialog();
-                    }
+					@Override
+					public void onError (FirebaseError firebaseError) {
 
-                    @Override
-                    public void onError(FirebaseError firebaseError) {
+						if (mProgress != null)
+							mProgress.dismiss ();
 
-                        if (mProgress != null)
-                            mProgress.dismiss();
+						Toast.makeText (SignUpPageActivity.this, "Email already exists or incorrect", Toast.LENGTH_SHORT).show ();
+					}
+				});
+			}
+		} else {
+			Utils.showLongMessage (Constants.CHECK_CONNECTION, SignUpPageActivity.this);
+		}
+	}
 
-                        Toast.makeText(SignUpPageActivity.this, "Email already exists or incorrect", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        } else {
-            Utils.showLongMessage(Constants.CHECK_CONNECTION, SignUpPageActivity.this);
-        }
-    }
+	public void compareEmail (final UserDetails userDetails, final String emailFromUserDetail, final String userdetailUid) {
 
-    public void compareEmail(final UserDetails userDetails, final String emailFromUserDetail, final String userdetailUid) {
+		ref.addValueEventListener (new ValueEventListener () {
+			@Override
+			public void onDataChange (DataSnapshot dataSnapshot) {
 
-        ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+				for (DataSnapshot users : dataSnapshot.getChildren ()) {
 
-                for (DataSnapshot users : dataSnapshot.getChildren()) {
+					String email = (String) users.child ("users").child ("email").getValue ();
 
-                    String email = (String) users.child("users").child("email").getValue();
+					if (emailFromUserDetail.trim ().equalsIgnoreCase (email)) {
 
-                    if (emailFromUserDetail.trim().equalsIgnoreCase(email)) {
+						Utils.showShortToast ("Email already exist!", SignUpPageActivity.this);
+					} else {
+						userRef.child (userdetailUid).setValue (userDetails);
+					}
+				}
+			}
 
-                        Utils.showShortToast("Email already exist!", SignUpPageActivity.this);
-                    } else {
-                        userRef.child(userdetailUid).setValue(userDetails);
-                    }
-                }
-            }
+			@Override
+			public void onCancelled (FirebaseError firebaseError) {
 
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
+			}
+		});
 
-            }
-        });
+	}
 
-    }
+	private boolean isPasswordValid (String password) {
+		return password.length () > 3;
+	}
 
-    private boolean isPasswordValid(String password) {
-        return password.length() > 3;
-    }
+	//    public
+	public void alertDialog () {
+		AlertDialog.Builder builder = new android.app.AlertDialog.Builder (SignUpPageActivity.this);
+		builder.setTitle ("Do you want to register a school?");
 
-//    public
-    public void alertDialog() {
-        AlertDialog.Builder builder = new android.app.AlertDialog.Builder(SignUpPageActivity.this);
-        builder.setTitle("Do you want to register a school?");
+		builder.setPositiveButton ("Yes", new DialogInterface.OnClickListener () {
+			@Override
+			public void onClick (DialogInterface dialog, int which) {
+				startActivity (new Intent (SignUpPageActivity.this, RegistrationActivity.class));
 
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(new Intent(SignUpPageActivity.this, RegistrationActivity.class));
+			}
+		});
 
-            }
-        });
+		builder.setNegativeButton ("No", new DialogInterface.OnClickListener () {
+			@Override
+			public void onClick (DialogInterface dialog, int which) {
+				startActivity (new Intent (SignUpPageActivity.this, ViewPageActivity.class));
 
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(new Intent(SignUpPageActivity.this, ViewPageActivity.class));
-
-            }
-        });
+			}
+		});
 
 
-        builder.create();
-        builder.show();
-    }
+		builder.create ();
+		builder.show ();
+	}
 
-    public void loginPage(View view) {
-        startActivity(new Intent(SignUpPageActivity.this, LoginActivity.class));
-    }
+	public void loginPage (View view) {
+		startActivity (new Intent (SignUpPageActivity.this, LoginActivity.class));
+	}
 
 }
